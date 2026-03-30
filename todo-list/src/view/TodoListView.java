d1b8995d4772ab326641710bbc371bd5b9709b75
rochiekop <rochieko11@gmail.com>
2026-03-30 21:02:37 +0700

    add view

--- todo-list/src/view/TodoListView.java
+++ todo-list/src/view/TodoListView.java
@@ -1,8 +1,37 @@
 package view;
 
-public class TodoListView {
-    public void displayTodoList(){
+import service.TodoListService;
+import util.InputUtil;
 
+public class TodoListView {
+
+    private TodoListService todoListService;
+
+
+    public TodoListView(TodoListService todoListService) {
+        this.todoListService = todoListService;
+    }
+
+    public void displayTodoList(){
+        while (true) {
+            todoListService.displayTodoList();
+            System.out.println("PILIHAN : ");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Exit");
+
+            var pilihan = InputUtil.input("Pilih");
+
+            if (pilihan.equals("1")) {
+                addTodoList();
+            } else if (pilihan.equals("2")) {
+                removeTodoList();
+            } else if (pilihan.equals("x")) {
+                break;
+            } else {
+                System.out.println("Pilihan tidak tersedia");
+            }
+        }
     }
 
     public void addTodoList(){
